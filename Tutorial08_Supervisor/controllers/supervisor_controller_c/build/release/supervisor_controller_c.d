9b587320a25c24abb318f36ffae1ac6bc548f7f6
ZCoolZ <50166062+ZcoolZBrony@users.noreply.github.com>
2023-01-19 17:06:02 +0700

    upload 2

---
+++ Tutorial08_Supervisor/controllers/supervisor_controller_c/build/release/supervisor_controller_c.d
@@ -0,0 +1,6 @@
+build/release/supervisor_controller_c.o: supervisor_controller_c.c \
+ C:\Users\user\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\user\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\user\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\user\AppData\Local\Programs\Webots/include/controller/c/webots/supervisor.h \
+ C:\Users\user\AppData\Local\Programs\Webots/include/controller/c/webots/contact_point.h
